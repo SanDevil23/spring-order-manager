e77374954c2fff06091b789b8ff790c643d641a7
Sankalp Adhya <101995802+SanDevil23@users.noreply.github.com>
2025-08-05 21:45:08 +0530

    added Lombok support to the project

---
+++ src/main/java/com/order_mgr/order_service/model/Order.java
@@ -0,0 +1,24 @@
+package com.order_mgr.order_service.model;
+
+import jakarta.persistence.*;
+import lombok.AllArgsConstructor;
+import lombok.Getter;
+import lombok.NoArgsConstructor;
+import lombok.Setter;
+
+import java.util.Date;
+
+@Getter
+@Setter
+@NoArgsConstructor
+@AllArgsConstructor
+@Entity
+@Table(name = "order")
+public class Order {
+    @Id
+    @GeneratedValue(strategy = GenerationType.AUTO)
+    private int orderID;
+    private int userID;
+    private String status;
+    private Date date;
+}
