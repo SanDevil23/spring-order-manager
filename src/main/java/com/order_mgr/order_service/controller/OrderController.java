fa647ee3bfc20ccd4ae758316c32f5423511a79d
Sankalp Adhya <101995802+SanDevil23@users.noreply.github.com>
2025-08-06 00:17:24 +0530

    controllers added for mapping the endpoints

---
+++ src/main/java/com/order_mgr/order_service/controller/OrderController.java
@@ -0,0 +1,40 @@
+package com.order_mgr.order_service.controller;
+
+import com.order_mgr.order_service.model.Order;
+import com.order_mgr.order_service.service.IOrderService;
+import com.order_mgr.order_service.utils.ApiResponse;
+import lombok.RequiredArgsConstructor;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.http.ResponseEntity;
+import org.springframework.web.bind.annotation.*;
+
+import java.util.List;
+
+@CrossOrigin("*")   // CORS resolution
+@RequiredArgsConstructor
+@RestController
+@RequestMapping("${api.prefix}/orders")
+public class OrderController {
+    private final IOrderService orderInterface;
+
+    @PostMapping("/add")
+    public ResponseEntity<ApiResponse> createOrder(@RequestBody Order order){
+        try{
+            orderInterface.createOrder(order);
+            return ResponseEntity.ok(new ApiResponse("Order Created Successfully", order));
+        } catch (Exception e){
+            return ResponseEntity.status(500).body(new ApiResponse("Order creation failed", null));
+        }
+    }
+
+    @GetMapping("/all")
+    public ResponseEntity<ApiResponse> fetchAllOrders(){
+        try {
+            List<Order> orderList = orderInterface.fetchAllOrders();
+            return ResponseEntity.ok(new ApiResponse("Order List", orderList));
+        } catch (Exception e) {
+            return ResponseEntity.status(500).body(new ApiResponse("Failed to fetch orders", null));
+        }
+    }
+
+}
