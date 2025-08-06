cd7dbc631d36a1e9c0ee756a82592482c2cb7ccf
Sankalp Adhya <101995802+SanDevil23@users.noreply.github.com>
2025-08-06 13:25:52 +0530

    configuration added for connecting mysql DB

--- src/main/java/com/order_mgr/order_service/model/Order.java
+++ src/main/java/com/order_mgr/order_service/model/Order.java
@@ -14,12 +14,15 @@ import java.util.Date;
 @NoArgsConstructor
 @AllArgsConstructor
 @Entity
-@Table(name = "order")
+@Table(name = "orderMgr")
 public class Order {
     @Id
     @GeneratedValue(strategy = GenerationType.AUTO)
     private int orderID;
     private int userID;
+
+    @Enumerated(EnumType.STRING)        // mapping enum into varchar
     private OrderStatus status;
+
     private Date date;
 }
