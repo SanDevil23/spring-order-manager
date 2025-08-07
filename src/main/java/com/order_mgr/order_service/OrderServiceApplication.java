e16137953c05fd852bf1e8c5c86ea56d552fe22c
Sankalp Adhya <101995802+SanDevil23@users.noreply.github.com>
2025-08-07 20:06:19 +0530

    adding log4j support with slf4j

--- src/main/java/com/order_mgr/order_service/OrderServiceApplication.java
+++ src/main/java/com/order_mgr/order_service/OrderServiceApplication.java
@@ -1,13 +1,16 @@
 package com.order_mgr.order_service;
 
+import org.slf4j.Logger;
+import org.slf4j.LoggerFactory;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 
 @SpringBootApplication
 public class OrderServiceApplication {
-
+	private static final Logger logger = LoggerFactory.getLogger(OrderServiceApplication.class);
 	public static void main(String[] args) {
 		SpringApplication.run(OrderServiceApplication.class, args);
+		logger.info("Application started");
 	}
 
 }
--- src/main/java/com/order_mgr/order_service/controller/OrderController.java
+++ src/main/java/com/order_mgr/order_service/controller/OrderController.java
@@ -4,6 +4,8 @@ import com.order_mgr.order_service.model.Order;
 import com.order_mgr.order_service.service.IOrderService;
 import com.order_mgr.order_service.utils.ApiResponse;
 import lombok.RequiredArgsConstructor;
+import org.slf4j.Logger;
+import org.slf4j.LoggerFactory;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.http.ResponseEntity;
 import org.springframework.web.bind.annotation.*;
@@ -15,14 +17,17 @@ import java.util.List;
 @RestController
 @RequestMapping("${api.prefix}/orders")
 public class OrderController {
+//    private static final Logger logger = LoggerFactory.getLogger(OrderController.class);
     private final IOrderService orderInterface;
 
     @PostMapping("/add")
     public ResponseEntity<ApiResponse> createOrder(@RequestBody Order order){
         try{
             orderInterface.createOrder(order);
+//            logger.info("Order placed successfully");
             return ResponseEntity.ok(new ApiResponse("Order Created Successfully", order));
         } catch (Exception e){
+//            logger.error("Error while creating new order");
             return ResponseEntity.status(500).body(new ApiResponse("Order creation failed", null));
         }
     }
