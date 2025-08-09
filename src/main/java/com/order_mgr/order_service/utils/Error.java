9f5a2bf1717d07a372ae56f33119142225bac295
Sankalp Adhya <101995802+SanDevil23@users.noreply.github.com>
2025-08-10 00:26:41 +0530

    methods for tracing some thrown exceptions

--- src/main/java/com/order_mgr/order_service/utils/Error.java
+++ src/main/java/com/order_mgr/order_service/utils/Error.java
@@ -4,6 +4,10 @@ import lombok.AllArgsConstructor;
 import lombok.Getter;
 import lombok.NoArgsConstructor;
 import lombok.Setter;
+import org.antlr.v4.runtime.misc.NotNull;
+
+import java.io.PrintWriter;
+import java.io.StringWriter;
 
 @Getter
 @Setter
@@ -12,4 +16,19 @@ import lombok.Setter;
 public class Error {
     private String message;
     private int httpStatusCode;
+
+    public static Throwable getRootCause(Throwable throwable) {
+        Throwable cause = throwable;
+        while (cause.getCause() != null && cause.getCause() != cause) {
+            cause = cause.getCause();
+        }
+        return cause;
+    }
+
+    public static String getStackTraceAsString(Throwable throwable) {
+        StringWriter sw = new StringWriter();
+        PrintWriter pw = new PrintWriter(sw);
+        throwable.printStackTrace(pw);
+        return sw.toString();
+    }
 }
