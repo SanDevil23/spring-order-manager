ea1dd2be46f01484768a11ea9f97cd48209bd283
Sankalp Adhya <101995802+SanDevil23@users.noreply.github.com>
2025-08-15 01:52:27 +0530

    config for oauth2 login

--- src/main/java/com/order_mgr/order_service/security/SecurityConfig.java
+++ src/main/java/com/order_mgr/order_service/security/SecurityConfig.java
@@ -1,28 +1,27 @@
 package com.order_mgr.order_service.security;
 
+
 import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
+import org.springframework.security.config.Customizer;
 import org.springframework.security.config.annotation.web.builders.HttpSecurity;
 import org.springframework.security.config.annotation.web.configuration.EnableWebSecurity;
-import org.springframework.security.config.annotation.web.configurers.LogoutConfigurer;
+import org.springframework.security.config.annotation.web.configurers.AbstractHttpConfigurer;
 import org.springframework.security.web.SecurityFilterChain;
 
 @Configuration
 @EnableWebSecurity
-public class SecurityConfig{
+public class SecurityConfig {
 
     @Bean
     public SecurityFilterChain filterChain(HttpSecurity http) throws Exception {
         http
-                .authorizeHttpRequests(authz -> authz
-                        .requestMatchers("/oauth2/**").permitAll()
+                .csrf(AbstractHttpConfigurer::disable)
+                .authorizeHttpRequests(auth -> auth
+                        .requestMatchers("/", "/login**", "/error").permitAll()
                         .anyRequest().authenticated()
                 )
-                .formLogin(form -> form
-                        .loginPage("/login")
-                        .permitAll()
-                )
-                .logout(LogoutConfigurer::permitAll);
+                .oauth2Login(Customizer.withDefaults()); // Enables GitHub login
 
         return http.build();
     }
