3cb98597cfb2ebc9733928e69cd21c1a0e2270a7
Sankalp Adhya <101995802+SanDevil23@users.noreply.github.com>
2025-08-14 17:00:45 +0530

    aspectJ support enabled

--- src/main/java/com/order_mgr/order_service/utils/LoggingAspect.java
+++ src/main/java/com/order_mgr/order_service/utils/LoggingAspect.java
@@ -5,23 +5,31 @@ import org.aspectj.lang.annotation.AfterReturning;
 import org.aspectj.lang.annotation.AfterThrowing;
 import org.aspectj.lang.annotation.Aspect;
 import org.aspectj.lang.annotation.Before;
+import org.slf4j.Logger;
+import org.slf4j.LoggerFactory;
 import org.springframework.stereotype.Component;
 
 @Aspect
 @Component
 public class LoggingAspect {
+
+    private static final Logger logger = LoggerFactory.getLogger(LoggingAspect.class);
+
     @Before("execution(* com.order_mgr.order_service.*.*(..))")
     public void logBeforeMethodExecution(JoinPoint joinPoint){
-        System.out.println("Entering method: " + joinPoint.getSignature().getName());
+        logger.info("Entering method: {} ", joinPoint.getSignature().getName());
     }
 
     @AfterReturning("execution(* com.order_mgr.order_service.*.*(..))")
     public void logAfterMethodExecution(JoinPoint joinPoint){
-        System.out.println("Exiting method: " + joinPoint.getSignature().getName());
+        logger.info("Exiting Method: {}", joinPoint.getSignature().getName());
     }
 
     @AfterThrowing(pointcut = "execution(* com.order_mgr.order_service.*.*(..))", throwing = "error")
     public void logAfterException(JoinPoint joinPoint, Throwable error) {
-        System.out.println("Exception in method: " + joinPoint.getSignature().getName() + " with error: " + error.getMessage());
+        logger.error("Exception in method: {} with error: {}",
+                joinPoint.getSignature().getName(),
+                error.getMessage(),
+                error);  // This also logs the full stack trace
     }
 }
