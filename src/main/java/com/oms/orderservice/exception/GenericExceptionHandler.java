3cdcb419d18673cdf7e8d684e5b2406e9c51cce3
khawaja-abdullah <khawaja.abdullah.ansar@gmail.com>
2025-08-15 15:29:30 +0500

    minor fixes

--- src/main/java/com/oms/orderservice/exception/GenericExceptionHandler.java
+++ src/main/java/com/oms/orderservice/exception/GenericExceptionHandler.java
@@ -2,27 +2,29 @@ package com.oms.orderservice.exception;
 
 import com.oms.orderservice.util.ApiResponse;
 import jakarta.persistence.EntityNotFoundException;
-import lombok.extern.slf4j.Slf4j;
+import org.slf4j.Logger;
+import org.slf4j.LoggerFactory;
 import org.springframework.http.HttpStatus;
 import org.springframework.http.ResponseEntity;
 import org.springframework.web.bind.annotation.ControllerAdvice;
 import org.springframework.web.bind.annotation.ExceptionHandler;
 
-@Slf4j
 @ControllerAdvice
 public class GenericExceptionHandler {
 
+    private static final Logger LOGGER = LoggerFactory.getLogger(GenericExceptionHandler.class);
+
     @ExceptionHandler(value = IllegalArgumentException.class)
     public ResponseEntity<ApiResponse> handleIllegalArgumentException(IllegalArgumentException e) {
-        log.error(e.getMessage());
+        LOGGER.error(e.getMessage());
         return ResponseEntity
-                .status(HttpStatus.UNPROCESSABLE_ENTITY)
+                .status(HttpStatus.BAD_REQUEST)
                 .body(ApiResponse.error("Invalid request parameters"));
     }
 
     @ExceptionHandler(value = EntityNotFoundException.class)
     public ResponseEntity<ApiResponse> handleEntityNotFoundException(EntityNotFoundException e) {
-        log.error(e.getMessage());
+        LOGGER.error(e.getMessage());
         return ResponseEntity
                 .status(HttpStatus.NOT_FOUND)
                 .body(ApiResponse.error("Resource not found"));
@@ -30,7 +32,7 @@ public class GenericExceptionHandler {
 
     @ExceptionHandler(value = Exception.class)
     public ResponseEntity<ApiResponse> handleException(Exception e) {
-        log.error(e.getMessage());
+        LOGGER.error(e.getMessage());
         return ResponseEntity
                 .status(HttpStatus.INTERNAL_SERVER_ERROR)
                 .body(ApiResponse.error("Something went wrong"));
