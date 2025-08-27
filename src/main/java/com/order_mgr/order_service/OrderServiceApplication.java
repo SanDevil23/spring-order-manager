2510ee16f35cb4bde53170f64ff2c1aa21ceaaba
Sankalp Adhya <101995802+SanDevil23@users.noreply.github.com>
2025-08-28 00:56:32 +0530

    adding open api definition to main

--- src/main/java/com/order_mgr/order_service/OrderServiceApplication.java
+++ src/main/java/com/order_mgr/order_service/OrderServiceApplication.java
@@ -1,10 +1,15 @@
 package com.order_mgr.order_service;
 
+import io.swagger.v3.oas.annotations.OpenAPIDefinition;
+import io.swagger.v3.oas.annotations.info.Info;
 import org.slf4j.Logger;
 import org.slf4j.LoggerFactory;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 
+@OpenAPIDefinition(
+		info = @Info(title = "OMS API", version = "1.0", description = "API documentation for clients and developers")
+)
 @SpringBootApplication
 public class OrderServiceApplication {
 	private static final Logger logger = LoggerFactory.getLogger(OrderServiceApplication.class);
